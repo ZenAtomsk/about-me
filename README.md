708d8083633863e7a73c0bdc3161b574ea915873
ZenAtomsk <ricardo.b3788@gmail.com>
2020-09-30 22:51:02 -0700

    working on my loops. Think it's ok...for now

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ This purpose of this website is to quiz the user on fun facts to give the user b
 ### Author: Ricardo Barcenas
 
 ### Links and Resources
-* [rel.it](https://repl.it/)
+* [repl.it](https://repl.it/)
 * [CSS cheatsheet](https://overapi.com/css)
 * [MDN Web Docs](https://developer.mozilla.org/en-US/)
+* [Class mates](https://canvas.instructure.com/courses/2325182/users)
 
 ### Reflections and Comments
 * Talking about myself is extrememly uncomfortable for me.
 * VS code is very handy with the extensions provided, I am excited to learn more extension when their use becomes apparant.
+* Holy guacamole, I felt confident after class, then my brain let out a stinker. Getting it together though. I'm supposed to be frightened and nauseous,....right?
--- js/app.js
+++ js/app.js
@@ -1,86 +1,114 @@
 'use strict';
 
-// var userName = prompt('Hello, what is your name?');
-// alert('nice to meet you ' + userName);
+var keepingScore = 0
 
-// var questionUno = prompt('Is my name Ricardo?');
+var userName = prompt('Hello, what is your name?');
+alert('nice to meet you ' + userName);
 
-// if(questionUno.toLowerCase() === 'yes' || questionUno.toLowerCase() === 'y'){
-//     // console.log('You are correct!');
-//     alert('You are correct!');
-// } else {
-//     // console.log('Nope, wrong answer.');
-//     alert('Nope, wrong answer.');
-// }
+var questionUno = prompt('Is my name Ricardo?');
 
-
-// var questionTwo = prompt('Was my first language was English?');
-
-
-// if(questionTwo.toLowerCase() === 'no' || questionTwo.toLowerCase() === 'n'){
-//     // console.log('You are correct!');
-//     alert('You are correct!');
-// } else {
-//     // console.log('Nope, wrong answer.');
-//     alert('Nope, wrong answer.');
-// }
+if(questionUno.toLowerCase() === 'yes' || questionUno.toLowerCase() === 'y'){
+    // console.log('Question one, right answer.');
+    alert('You are correct!');
+    keepingScore++;
+} else {
+    // console.log('Question one, wrong answer..');
+    alert('Nope, wrong answer.');
+}
 
 
-// var questionThree = prompt('Did I get an associates degree in Las Vegas, Nevada?');
+var questionTwo = prompt('Was my first language was English?');
+
+if(questionTwo.toLowerCase() === 'no' || questionTwo.toLowerCase() === 'n'){
+    // console.log('Question two, right answer.');
+    alert('You are correct!');
+    keepingScore++;
+} else {
+    // console.log('Question two, wrong answer.');
+    alert('Nope, wrong answer.');
+}
 
 
-// if(questionThree.toLowerCase() === 'no' || questionThree.toLowerCase() === 'n'){
-//     // console.log('You are correct!');
-//     alert('You are correct!');
-// } else {
-//     // console.log('Nope, wrong answer.');
-//     alert('Nope, wrong answer.');
-// }
+var questionThree = prompt('Did I get an associates degree in Las Vegas, Nevada?');
+
+if(questionThree.toLowerCase() === 'no' || questionThree.toLowerCase() === 'n'){
+    // console.log('Question three, right answer.');
+    alert('You are correct!');
+    keepingScore++;
+} else {
+    // console.log('Question three, wrong answer..');
+    alert('Nope, wrong answer.');
+}
 
 
-// var questionFour = prompt('Did I work at a family practice in Las Vegas?');
+var questionFour = prompt('Did I work at a family practice in Las Vegas?');
+
+if(questionFour.toLowerCase() === 'yes' || questionFour.toLowerCase() === 'y'){
+    // console.log('Question four, right answer.');
+    alert('You are correct!');
+    keepingScore++;
+} else {
+    // console.log('Question four, wrong answer.');
+    alert('Nope, wrong answer.');
+}
 
 
-// if(questionFour.toLowerCase() === 'yes' || questionFour.toLowerCase() === 'y'){
-//     // console.log('You are correct!');
-//     alert('You are correct!');
-// } else {
-//     // console.log('Nope, wrong answer.');
-//     alert('Nope, wrong answer.');
-// }
+var questionFive = prompt('Did my wife and I start dating while playing Pokemon Go?');
+
+if(questionFive.toLowerCase() === 'yes' || questionFive.toLowerCase() === 'y'){
+    // console.log('Question five, right answer.');
+    alert('You are correct!');
+    keepingScore++;
+} else {
+    // console.log('Question five, wrong answer.');
+    alert('Nope, wrong answer.');
+}
+
+document.write('Welcome to my \"About Me\" ' + userName);
+alert('Welcome to my \"About Me\" ' + userName);
 
 
-// var questionFive = prompt('Did my wife and I start dating while playing Pokemon Go?');
+//question number six
+
+var questionSix;
+var i = 0;
+var favNum = 7;
+
+while (questionSix != favNum && i < 4){
+    questionSix = prompt('What is my favorite number?');
+    if (parseInt(questionSix)===favNum){
+        // console.log('Question six, right answer.');
+        alert('You got it!');
+        keepingScore++;
+        break;
+    } else if ( questionSix > favNum){
+        // console.log('Question six, wrong answer, too high.');
+        prompt('Too high! Try again');
+        i++;
+    } else if (questionSix < favNum){
+        // console.log('Question six, wrong answer, too low.');
+        prompt('Lowball, guess again');
+        i++;
+    }
+}
 
 
-// if(questionFive.toLowerCase() === 'yes' || questionFive.toLowerCase() === 'y'){
-//     // console.log('You are correct!');
-//     alert('You are correct!');
-// } else {
-//     // console.log('Nope, wrong answer.');
-//     alert('Nope, wrong answer.');
-// }
+//question number seven....is it over yet??
 
-// document.write('Welcome to my \"About Me\" ' + userName);
-// alert('Welcome to my \"About Me\" ' + userName);
+var pcGames = ['monster hunter', 'league of legends', 'overwatch'];
+for (var i = 0; i < 6; i++){
+    var questionSeven = prompt('Can you name one of my favorite PC games?')
+    if (questionSeven.toLowerCase() === 'monster hunter' || questionSeven.toLowerCase() === 'league of legends' || questionSeven.toLowerCase() === 'overwatch'){
+        // console.log('Question seven was correct.');
+        alert('Fun game!! I love it!');
+        keepingScore++;
+        break;
+} else alert('Nope, but I should check that out. Guess again.');
+        // console.log('Question seven, wrong answer.');
+}
+if (i = 6 && questionSeven.toLowerCase !== pcGames){
+    alert('ALL of the correct answers; Monster Hunter, League of Legends and Overwatch')
+    // console.log('ALL of the correct answers; Monster Hunter, League of Legends and Overwatch');
+}
 
-
-// var questionSix = 0;
-// var i = 0;
-// var favNum = 7;
-// var chances = 4;
-
-// while (questionSix != favNum && i < chances){
-//     questionSix = prompt('What is my favorite number?');
-//     if (questionSix === favNum) {
-//         alert('You got it!');
-//     } else if ( questionSix > favNum){
-//         alert('Too high! Try again');
-//         i++;
-//     } else if (questionSix < favNum){
-//         alert('Lowball, guess again');
-//         i++;
-//     }
-// }
-
-
+alert('Tallying up your score now......beep booop boop beeeeeeeeeep............beep...THE RESULTS ARE IN!!!! badadadadadadadadadada...You got ' + keepingScore +'/7 answers correct!!!! Don\'t you feel like you know me now? \'Till next time Space Cowboy.')
