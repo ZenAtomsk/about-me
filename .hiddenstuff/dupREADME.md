9b87821592b9ec81bddb99e3faac35c360780a01
ZenAtomsk <ricardo.b3788@gmail.com>
2020-10-02 23:03:50 -0700

    removing comments

---
+++ .hiddenstuff/dupREADME.md
@@ -0,0 +1,19 @@
+# LAB - 02
+
+## About Me
+
+This purpose of this website is to quiz the user on fun facts to give the user basic information about who I am and where I came from.
+
+### Author: Ricardo Barcenas
+
+### Links and Resources
+* [repl.it](https://repl.it/)
+* [CSS cheatsheet](https://overapi.com/css)
+* [MDN Web Docs](https://developer.mozilla.org/en-US/)
+* [Class mates](https://canvas.instructure.com/courses/2325182/users)
+
+
+### Reflections and Comments
+* Talking about myself is extrememly uncomfortable for me.
+* VS code is very handy with the extensions provided, I am excited to learn more extension when their use becomes apparant.
+* Holy guacamole, I felt confident after class, then my brain let out a stinker. Getting it together though. I'm supposed to be frightened and nauseous,....right?
--- .hiddenstuff/dupapp.js
+++ .hiddenstuff/dupapp.js
@@ -11,9 +11,11 @@ function question1() {
     var questionUno = prompt('Is my name Ricardo?');
 
     if(questionUno.toLowerCase() === 'yes' || questionUno.toLowerCase() === 'y'){
+        // console.log('Question one, right answer.');
         alert('You are correct!');
         keepingScore++;
     } else {
+        // console.log('Question one, wrong answer..');
         alert('Nope, wrong answer.');
     }
 }
@@ -22,9 +24,11 @@ function question2() {
     var questionTwo = prompt('Was my first language was English?');
 
     if(questionTwo.toLowerCase() === 'no' || questionTwo.toLowerCase() === 'n'){
+        // console.log('Question two, right answer.');
         alert('You are correct!');
         keepingScore++;
     } else {
+        // console.log('Question two, wrong answer.');
         alert('Nope, wrong answer.');
     }
 }
@@ -33,9 +37,11 @@ function question3() {
     var questionThree = prompt('Did I get an associates degree in Las Vegas, Nevada?');
 
     if(questionThree.toLowerCase() === 'no' || questionThree.toLowerCase() === 'n'){
+        // console.log('Question three, right answer.');
         alert('You are correct!');
         keepingScore++;
     } else {
+        // console.log('Question three, wrong answer..');
         alert('Nope, wrong answer.');
     }
 }
@@ -44,9 +50,11 @@ function question4() {
     var questionFour = prompt('Did I work at a family practice in Las Vegas?');
 
     if(questionFour.toLowerCase() === 'yes' || questionFour.toLowerCase() === 'y'){
+        // console.log('Question four, right answer.');
         alert('You are correct!');
         keepingScore++;
     } else {
+        // console.log('Question four, wrong answer.');
         alert('Nope, wrong answer.');
     }
 }
@@ -55,9 +63,11 @@ function question5() {
     var questionFive = prompt('Did my wife and I start dating while playing Pokemon Go?');
 
     if(questionFive.toLowerCase() === 'yes' || questionFive.toLowerCase() === 'y'){
+        // console.log('Question five, right answer.');
         alert('You are correct!');
         keepingScore++;
     } else {
+        // console.log('Question five, wrong answer.');
         alert('Nope, wrong answer.');
     }
 }
@@ -76,13 +86,16 @@ function question6() {
     while (i < 4){
         questionSix = prompt('What is my favorite number?');
         if (parseInt(questionSix)===favNum){
+            // console.log('Question six, right answer.');
             alert('You got it!');
             keepingScore++;
             break;
         } else if (parseInt(questionSix) > favNum){
+            // console.log('Question six, wrong answer, too high.');
             alert('Too high! Try again');
             i++;
         } else if (parseInt(questionSix) < favNum){
+            // console.log('Question six, wrong answer, too low.');
             alert('Lowball, guess again');
             i++;
         }
@@ -90,6 +103,7 @@ function question6() {
 }
 
 // question number seven....is it over yet??
+// questionSeven === 'monster hunter' || questionSeven === 'league of legends' || questionSeven === 'overwatch'
 
 function question7() {
     var pcGames = ['monster hunter', 'league of legends', 'overwatch'];
@@ -100,6 +114,7 @@ function question7() {
             console.log(questionSeven);
             console.log('guess made');
             if (questionSeven === pcGames[j]) {
+                // console.log('Question seven was correct.');
                 console.log(questionSeven);
                 console.log('correct answer');
                 alert('Fun game!! I love it!');
@@ -111,10 +126,12 @@ function question7() {
                 console.log('wrong answer');
                 alert('Nope, but I should check that out. Guess again.');
                 break;
+                // console.log('Question seven, wrong answer.');
             } else if (questionSeven !== pcGames[j] && i === numberTries) {
                 console.log(questionSeven);
                 console.log('wrong last');
                 alert('ALL of the correct answers; Monster Hunter, League of Legends and Overwatch');
+                // console.log('ALL of the correct answers; Monster Hunter, League of Legends and Overwatch');
                 break;
             }
         }
@@ -135,3 +152,27 @@ question6();
 question7();
 
 alert('Tallying up your score now......beep booop boop beeeeeeeeeep............beep...THE RESULTS ARE IN!!!! badadadadadadadadadada...You got ' + keepingScore +'/7 answers correct!!!! Don\'t you feel like you know me now? \'Till next time Space Cowboy.');
+
+
+
+
+function sum(a, b, c) { //eslint-disable-line
+    var totalSum = a + b;
+    var c = 0;
+    var string = 'The sum of ' + a + ' and ' + b + ' is ' + totalSum + '.';
+    return [totalSum, string];
+  }
+
+function multiply(a, b, c) { //eslint-disable-line
+    var totalSum = a * b;
+    var c = 0;
+    var string = 'The product of ' + a + ' and ' + b + ' is ' + totalSum + '.';
+    return [totalSum, string];
+  }
+
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var totalSum = sum(a, b, c);
+    var totalProduct = multiply(a, b, c);
+    var string = a + ' and ' + b + ' and ' + c + " sum to " + totalSum + ".";
+    return [totalSum, totalProduct, string];
+}
